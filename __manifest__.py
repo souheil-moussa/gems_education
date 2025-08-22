206d550911a9d12475992dd99d166f32debe5af0
CharbelManuel <charbelmanuel0@gmail.com>
2025-08-22 11:33:35 +0300

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Gems Educatiom',
+    'version': '1.0',
+    'depends': [],
+    'author': 'Souheil,Kevin,Charbel',
+    'category': 'Inventory',
+    'description': 'Manage Gems education system',
+    'data': [],
+    'sequence': '1',
+    'installable': True,
+    'application': True,
+}
